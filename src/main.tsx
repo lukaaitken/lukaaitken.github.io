e347f2ac87b2729068d402a31d44a70c3e509749
Luka Aitken <aitkenl19@mytru.ca>
2025-12-04 21:35:02 -0800

    Project init

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+/**
+ *  @copyright 2025 Luka Aitken
+ *  @license Apache-2.0
+ */
+
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/index.css'
+import { App } from '@/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
